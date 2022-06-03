8ab3eac5edd228c3b4cffc6cba7c0a9b912282aa
Vincent Langat <kimutailangat033@gmail.com>
2022-06-03 22:31:27 +0300

    commit tree 62752c8706046c8748a1559f786bc5d2b257f756

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Code your solutions in this file
+// returns an array of thank you messages for each name provided to the function
 function writeCards(name){
     let message=[]
     for ( let i=0; i<name.length; i++ ){
@@ -6,11 +7,13 @@ function writeCards(name){
     }
     return message;
 }
+// invokes console.log once for each number, counting down from the number provided to zero
 function countDown(name){
     
     while ( name> 0){
         console.log(name)
         name--;
     }
+    // logs each number when counting down, starting from the number provided
     console.log(name)
 }
