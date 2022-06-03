417d8abae4b7294ced022f461904e203f1095db1
Vincent Langat <kimutailangat033@gmail.com>
2022-06-03 22:29:59 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(name){
+    let message=[]
+    for ( let i=0; i<name.length; i++ ){
+        message.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+    }
+    return message;
+}
+function countDown(name){
+    
+    while ( name> 0){
+        console.log(name)
+        name--;
+    }
+    console.log(name)
+}
